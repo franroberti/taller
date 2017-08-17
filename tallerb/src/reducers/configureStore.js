0cec80c9b58441ec3ee58a12114cc23b7f63a2f9
Mariano <mariano@live.com>
2017-08-17 15:28:06 -0300

    Agrego lista de clientes

--- tallerb/src/reducers/configureStore.js
+++ tallerb/src/reducers/configureStore.js
@@ -1,10 +1,6 @@
 import {combineReducers,applyMiddleware} from 'redux';
 import {messages} from './messages';
-<<<<<<< HEAD
 import {clients} from "./clients";
-=======
-import {clientsForm} from "./clientsForm";
->>>>>>> b07c2f36896c1d84695955d9c82727000e0dc680
 import {createStore} from 'redux';
 import thunk from 'redux-thunk';
 
@@ -12,12 +8,7 @@ import thunk from 'redux-thunk';
 
 
 export const reducers = combineReducers({
-<<<<<<< HEAD
     messages,clients
-=======
-    messages,
-    clientsForm
->>>>>>> b07c2f36896c1d84695955d9c82727000e0dc680
 });
 
 export const store = createStore(reducers, applyMiddleware(thunk));
